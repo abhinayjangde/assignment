e2ac3c9a7cce9a8e81fff5bb8dacd5a9cf84f41d
Abhinay Jangde <abhinayjangde@gmail.com>
2025-10-01 08:33:54 +0530

    add database connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 import app from "./src/app.js";
 import { config } from "./src/configs/config.js";
+import db from "./src/configs/db.js";
 
-app.listen(config.port, () => {
-    console.log(`Server is running on port http://localhost:${config.port}`);
-});
+
+const startServer = async () => {
+    await db();
+    const port = config.port;
+    app.listen(port, () => {
+        console.log(`Server is running on port http://localhost:${port}`);
+    });
+
+};
+
+startServer();
