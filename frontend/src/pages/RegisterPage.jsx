0cfa71a3559744ab4d38dcd1c11f5e7c78c8c62e
Abhinay Jangde <abhinayjangde@gmail.com>
2025-10-03 12:10:17 +0530

    add register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { Label } from "@radix-ui/react-label";
+import { Link } from "react-router";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <div className={cn("flex flex-col gap-6")}>
+          <Card className="overflow-hidden p-0">
+            <CardContent className="grid p-0 md:grid-cols-2">
+              <form className="p-6 md:p-8">
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col items-center text-center">
+                    <h1 className="text-2xl font-bold">Join Us</h1>
+                    <p className="text-muted-foreground text-balance">
+                      Create an account to continue
+                    </p>
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="name">Name</Label>
+                    <Input
+                      id="name"
+                      type="name"
+                      placeholder="John Doe"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="password">Password</Label>
+
+                    <Input id="password" type="password" required />
+                  </div>
+                  <Button type="submit" className="w-full">
+                    Register
+                  </Button>
+
+                  <div className="text-center text-sm">
+                    Already have an account?{" "}
+                    <Link
+                      to={"/login"}
+                      className="underline underline-offset-4"
+                    >
+                      Login
+                    </Link>
+                  </div>
+                </div>
+              </form>
+              <div className="bg-muted relative hidden md:block">
+                <img
+                  src="https://dummyimage.com/300x200"
+                  alt="Image"
+                  className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
+              </div>
+            </CardContent>
+          </Card>
+          <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+            By clicking continue, you agree to our{" "}
+            <Link to="#" className="underline underline-offset-4">
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link to="#" className="underline underline-offset-4">
+              Privacy Policy
+            </Link>
+            .
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
