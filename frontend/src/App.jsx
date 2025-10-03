07ae6b13e7b3210901862e5f70b76bead773fb10
Abhinay Jangde <abhinayjangde@gmail.com>
2025-10-03 09:49:35 +0530

    add shadcn in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
     <div>
       <h1 className="text-4xl font-bold underline">Frontend</h1>
-
+      <Button>Button</Button>
     </div>
   )
 }
