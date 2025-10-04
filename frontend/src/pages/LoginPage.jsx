1bd709f6f0a0387b22a79cb49a59a2902b1a9056
Abhinay Jangde <abhinayjangde@gmail.com>
2025-10-04 17:19:17 +0530

    add tanstack query

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,16 +3,26 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { Label } from "@radix-ui/react-label";
+import { useRef } from "react";
 import { Link } from "react-router";
 
 const LoginPage = () => {
+  const passwordRef = useRef(null);
+  const emailRef = useRef(null);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log("Login submitted:", { email, password });
+  };
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <div className={cn("flex flex-col gap-6")}>
           <Card className="overflow-hidden p-0">
             <CardContent className="grid p-0 md:grid-cols-2">
-              <form className="p-6 md:p-8">
+              <form onSubmit={handleLoginSubmit} className="p-6 md:p-8">
                 <div className="flex flex-col gap-6">
                   <div className="flex flex-col items-center text-center">
                     <h1 className="text-2xl font-bold">Welcome back</h1>
@@ -23,6 +33,7 @@ const LoginPage = () => {
                   <div className="grid gap-3">
                     <Label htmlFor="email">Email</Label>
                     <Input
+                      ref={emailRef}
                       id="email"
                       type="email"
                       placeholder="m@example.com"
@@ -39,7 +50,12 @@ const LoginPage = () => {
                         Forgot your password?
                       </Link>
                     </div>
-                    <Input id="password" type="password" required />
+                    <Input
+                      ref={passwordRef}
+                      id="password"
+                      type="password"
+                      required
+                    />
                   </div>
                   <Button type="submit" className="w-full">
                     Login
